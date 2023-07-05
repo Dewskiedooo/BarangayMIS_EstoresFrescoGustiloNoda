4a50515bac92b23f33c8f002bfab616e77b9f7bd
Dewskiedooo <dewey.fresco@cvsu.edu.ph>
2023-07-05 19:32:42 +0800

    Please enter the commit message for your changes. Lines starting   with ' ' will be ignored, and an empty message aborts the commit.
     	modified:   app/Controllers/Home.php
     	modified:   app/Views/dashboard_view.php
     	new file:   app/Views/layouts/base.php
     	new file:   app/Views/layouts/bootstrap.php
     	new file:   app/Views/layouts/header.php
     	new file:   app/Views/layouts/jquery.php
     	new file:   app/Views/layouts/logo.php
     	modified:   app/Views/layouts/main.php
     	modified:   app/Views/layouts/navbar.php
     	new file:   app/Views/login_view.php
     	deleted:    writable/debugbar/debugbar_1687152975.060115.json
     	deleted:    writable/debugbar/debugbar_1687153351.359829.json
     	deleted:    writable/debugbar/debugbar_1687153352.814863.json
     	deleted:    writable/debugbar/debugbar_1687153419.302585.json
     	deleted:    writable/debugbar/debugbar_1687153421.083023.json
     	deleted:    writable/debugbar/debugbar_1687153453.682068.json
     	deleted:    writable/debugbar/debugbar_1687153521.669715.json
     	deleted:    writable/debugbar/debugbar_1687153523.609620.json
     	deleted:    writable/debugbar/debugbar_1687153609.521094.json
     	deleted:    writable/debugbar/debugbar_1687153611.787052.json
     	deleted:    writable/debugbar/debugbar_1687153732.053739.json
     	deleted:    writable/debugbar/debugbar_1687153733.843252.json
     	deleted:    writable/debugbar/debugbar_1687826205.242050.json
     	deleted:    writable/debugbar/debugbar_1687826206.471012.json
     	new file:   writable/debugbar/debugbar_1688554482.103178.json
     	new file:   writable/debugbar/debugbar_1688554616.199993.json
     	new file:   writable/debugbar/debugbar_1688555069.014925.json
     	renamed:    writable/debugbar/debugbar_1687825975.924640.json -> writable/debugbar/debugbar_1688555090.769584.json
     	renamed:    writable/debugbar/debugbar_1687153456.120303.json -> writable/debugbar/debugbar_1688555149.743370.json
     	renamed:    writable/debugbar/debugbar_1687826011.186730.json -> writable/debugbar/debugbar_1688555151.850400.json
     	renamed:    writable/debugbar/debugbar_1687153228.601129.json -> writable/debugbar/debugbar_1688555171.465266.json
     	new file:   writable/debugbar/debugbar_1688555172.912717.json
     	new file:   writable/debugbar/debugbar_1688555212.383692.json
     	new file:   writable/debugbar/debugbar_1688556027.722143.json
     	new file:   writable/debugbar/debugbar_1688556029.000762.json
     	new file:   writable/debugbar/debugbar_1688556177.899288.json
     	new file:   writable/debugbar/debugbar_1688556178.991345.json
     	new file:   writable/debugbar/debugbar_1688556363.571706.json
     	new file:   writable/debugbar/debugbar_1688556457.439998.json
     	new file:   writable/debugbar/debugbar_1688556461.145967.json
     	renamed:    writable/debugbar/debugbar_1687152973.602291.json -> writable/debugbar/debugbar_1688556567.667276.json
     	new file:   writable/debugbar/debugbar_1688556569.666498.json
     	new file:   writable/debugbar/debugbar_1688556747.182517.json
     	renamed:    writable/debugbar/debugbar_1687153227.148515.json -> writable/debugbar/debugbar_1688556749.044217.json
     	new file:   writable/logs/log-2023-07-05.log

--- app/Views/layouts/navbar.php
+++ app/Views/layouts/navbar.php
@@ -5,16 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?= $page_title;?></title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?php base_url()?>assets/AdminLTE-3.2.0/plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="<?php base_url()?>assets/AdminLTE-3.2.0/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="<?php base_url()?>assets/AdminLTE-3.2.0/dist/css/adminlte.min.css">
+  
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
   <div class="wrapper">
---
+++ app/Views/login_view.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?= $this->renderSection("title");?></title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php base_url()?>assets/AdminLTE-3.2.0/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="<?php base_url()?>assets/AdminLTE-3.2.0/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php base_url()?>assets/AdminLTE-3.2.0/dist/css/adminlte.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="<?php base_url()?>assets/AdminLTE-3.2.0/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  
+</head>
+
+        <!-- Input addon -->
+        <div class="card card-info">
+                    <div class="card-header">
+                        <h3 class="card-title">Login Form</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">@</span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Username">
+                        </div>
+
+                        <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                        </div>
+                        <input type="email" class="form-control" placeholder="Email">
+                        </div>
+
+                        
+                   
+            </div>
+
+<?= $this->extend("layouts/footer");?>
